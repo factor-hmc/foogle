4c2bc86da98221903570e2c9e59e61834b3dc86b
Cole K <cole-k@users.noreply.github.com>
2020-04-24 12:21:54 -0700

    fix edit distance bug

--- src/EditDistance.hs
+++ src/EditDistance.hs
@@ -5,16 +5,20 @@ import Data.Array
 
 -- | Configurable costs for the edit distance function.
 -- These can be adjusted based on the elements themselves.
+-- 
+-- The insert/delete costs are constants so that calculating the
+-- edit distance is slightly faster, but it's simple to change
+-- to make it depend on the element itself.
 data EditDistanceOptions a = EditDistanceOptions
-  { insertCost  :: a -> Int      -- ^ Cost to insert an element
-  , deleteCost  :: a -> Int      -- ^ Cost to delete an element
+  { insertCost  :: Int           -- ^ Cost to insert an element
+  , deleteCost  :: Int           -- ^ Cost to delete an element
   , replaceCost :: a -> a -> Int -- ^ Cost to replace one element by another
   }
 
 defaultEditDistanceOptions :: Eq a => EditDistanceOptions a
 defaultEditDistanceOptions = EditDistanceOptions
-  { insertCost  = const 1
-  , deleteCost  = const 1
+  { insertCost  = 1
+  , deleteCost  = 1
   , replaceCost = \x -> \y -> 
       if x == y 
         then 0
@@ -34,14 +38,12 @@ editDistance EditDistanceOptions{..} xs ys = table ! (m,n)
     table = array bnds [(ij, dist ij) | ij <- range bnds]
     bnds  = ((0,0),(m,n))
     
-    -- I'm not sure why, but changing these base cases to be the sum of the 
-    -- costs of the elements skews the search...
-    -- Will have to look into it later.
-    dist (0,j) = j -- sum [ insertCost (y ! yy) | yy <- [1,j] ]
-    dist (i,0) = i -- sum [ deleteCost (x ! xx) | xx <- [1,i] ]
+                                -- If the cost is a function, we can use the below
+    dist (0,j) = insertCost * j -- sum [ insertCost (y ! yy) | yy <- [1..j] ]
+    dist (i,0) = deleteCost * i -- sum [ deleteCost (x ! xx) | xx <- [1..i] ]
     dist (i,j) = minimum 
-      [ table ! (i-1,j) + deleteCost elem1
-      , table ! (i,j-1) + insertCost elem2
+      [ table ! (i-1,j) + deleteCost -- elem1
+      , table ! (i,j-1) + insertCost -- elem2
       , replaceCost elem1 elem2 + table ! (i-1,j-1)
       ]
       where
--- src/Search.hs
+++ src/Search.hs
@@ -11,40 +11,49 @@ import Types
 
 effectEditDistanceOptions :: EditDistanceOptions (EffVar Text)
 effectEditDistanceOptions = EditDistanceOptions
-  { insertCost = const 4
-  , deleteCost = const 4
-  -- This probably should recursively compare effects (e.g. for quotations)
+  { insertCost = 3
+  , deleteCost = 3
   , replaceCost = effVarCost
   }
 
 effVarCost :: EffVar Text -> EffVar Text -> Int
 effVarCost queryV effV= case (queryV, effV) of
-  (EffVar qv, EffVar ev)
-           | qv == ev                      -> -1
-           | qv `T.isInfixOf` ev           -> 0
-           | otherwise                     -> 2
-  (TypedEffVar qv [qtp], TypedEffVar ev etps)
-           | qv == ev && qtp `elem` etps   -> -2
-           | qtp `elem` etps               -> -1
-           | qv == ev                      -> 0
-           | qv `T.isInfixOf` ev           -> 1
-           | otherwise                     -> 2
-  -- Eventually, we may add support for this, but it's better to explicitly fail for now
-  (TypedEffVar qv _, TypedEffVar _ _) -> 
-    error "query effvar has more than one type (this shouldn't happen)"
+  (EffVar qv, EffVar ev) ->
+    let nameMatch = qv == ev
+        nameClose = qv `T.isInfixOf` ev
+     in if | nameMatch             -> -1
+           | nameClose             -> 0
+           | otherwise             -> 2
+  (TypedEffVar qv qtps, TypedEffVar ev etps) ->
+        -- If we're searching with a union, only one of them needs to match
+    let tpMatch   = any (`elem` etps) qtps
+        nameMatch = qv == ev
+        nameClose = qv `T.isInfixOf` ev
+     in if | nameClose && tpMatch -> -3
+           | tpMatch              -> -2
+           | nameMatch            -> 0
+           | nameClose            -> 1
+           | otherwise            -> 2
   (QuotEffVar qv qeff, QuotEffVar ev eeff) ->
     let c = cost qeff eeff
-     in if | qv == ev && c <= 0            -> c - 2
-           | qv `T.isInfixOf` ev && c <= 0 -> c - 1
-           | qv == ev                      -> 0
-           | qv `T.isInfixOf` ev           -> 1
-           | otherwise                     -> 2
+        nameMatch = qv == ev
+        nameClose = qv `T.isInfixOf` ev
+     in if | nameClose && c <= 0  -> c - 3
+           | c <= 0               -> c - 2
+           | nameMatch            -> 0
+           | nameClose            -> 1
+           | otherwise            -> 2
+     
+  -- EVar mismatches we'll usually treat as problems, but if the names are the same
+  -- we'll say they cost nothing.
   _ -> 
     let qName = effVarName queryV
         eName = effVarName effV
-     in if | qName == eName                -> 0
-           | qName `T.isInfixOf` eName     -> 1
-           | otherwise                     -> 2
+        nameMatch = qName == eName
+        nameClose = qName `T.isInfixOf` eName
+     in if | nameMatch            -> 0
+           | nameClose            -> 1
+           | otherwise            -> 2
 
 compareEffVars :: [EffVar Text] -> [EffVar Text] -> Int
 compareEffVars = editDistance effectEditDistanceOptions 
@@ -55,7 +64,7 @@ cost queryEff eff =
   + compareEffVars (effOut queryEff) (effOut eff)
 
 searchDB :: Int -> Effect Text -> [FactorWord Text] -> [FactorWord Text]
-searchDB numResults query db = take numResults $ sortOn calculateCost db
+searchDB numResults query db = take numResults $ sortOn calculateCost db -- sortOn fst . map ((,) <$> calculateCost <*> id) $ db
   where
     calculateCost FactorWord{..} = case wordEff of
       -- absurdly high costs for words without effects
